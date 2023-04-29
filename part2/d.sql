d915ac656b8b51f004d51cd1ae97dd966cf55634
Shubham Kc <shubhamkc1997@gmail.com>
2023-04-29 12:22:22 -0400

    Completed SQL Assignment

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,12 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+select users.first_name, users.last_name 
+from users
+join favorites
+where users.id == favorites.user_id and favorites.dog_id == (select dogs.id from dogs where dogs.name == "Zoey");
+
+-- I think if we have tons of data, join will run slow. This might be more efficient for larger databases. 
+-- another implementation
+-- select users.first_name, users.last_name
+-- from users
+-- where users.id == (select favorites.user_id from favorites where favorites.dog_id == (select dogs.id from dogs where dogs.name == "Zoey"));
